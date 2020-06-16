d14dbd145a421c62b04f1eb169c88fad2c74bedb
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-06-16 13:15:56 +0300

    (4 Gradients and Pattern Fill)

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 //select the canvas Element
-let theCanvas = document.getElementById('our-canvas')
+let theCanvas = document.getElementById('our-canvas'),
 
-//select context type
-theContext = theCanvas.getContext('2d')
+    //select context type
+    theContext = theCanvas.getContext('2d'),
+
+    //The Gradient
+    theGradient = theContext.createLinearGradient(0, 0, 0, 0);
+
+
+//Add Color Stops
+theGradient.addColorStop(0, "red");
+theGradient.addColorStop(1, "blue");
+
+
+//Select he Image
+
+let theImage = document.getElementById('our-image');
+
+//create The Pattern
+
+
+// let thePatern = theContext.createPattern(theImage, 'no-repeat')
+// let thePatern = theContext.createPattern(theImage, 'repeat')
+let thePatern = theContext.createPattern(theImage, 'repeat-x');
 
 //chose Fill style
-theContext.fillStyly = '#000';
+theContext.fillStyly = thePatern;
 
 //Draw Rectangle
 // theContext.fillRect(5, 0, 200, 150)
