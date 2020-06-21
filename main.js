2f51b2007a988b5c3013b56f1e796a23ad847894
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-06-21 13:05:01 +0300

    (10 Shadow )

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
 //select the canvas Element
 let theCanvas = document.getElementById('our-canvas'),
     //select context type
-    theContext = theCanvas.getContext('2d')
+    theContext = theCanvas.getContext('2d');
 
-//Full Path
-theCircle = 2 * Math.PI;
-//Begin The Path
-theContext.beginPath();
-
-//Border styling
-theContext.lineWidth = 8;
-theContext.strokeStyle = "red"
-
-// theContext.arc(300, 200, 150, 0, 5.28319) //6.28319 , true
-// theContext.arc(300, 200, 150, 0, 1 * Math.PI)
-// theContext.arc(300, 200, 150, 0, 1.5 * Math.PI)
-// theContext.arc(300, 200, 150, 0, 2 * Math.PI)
-// theContext.arc(300, 200, 150, 0, 0.5 * Math.PI)
-theContext.arc(300, 200, 150, 0, 0.7 * theCircle)
-
-
-//Draw The Path
-theContext.stroke();
+// theContext.shadowColor = "black";
+theContext.shadowColor = "rgba(0, 0, 0, .5)";
+theContext.shadowBlur = 10;
+theContext.shadowOffsetX = "20"; //"-20"
+theContext.shadowOffsetY = "-20"; //"-20"
+theContext.fillStyle = "green";
+theContext.fillRect(50, 50, 200, 100);
