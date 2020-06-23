61da7001b5ee83a20a3098c2576f7b8ef0c7b8f2
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-06-23 12:04:07 +0300

    (13 الرسمة الكاملة للسمايل)

--- main.js
+++ main.js
@@ -7,9 +7,31 @@ let theCanvas = document.getElementById('our-canvas'),
 theContext.fillStyle = '#666';
 theContext.fillRect(0, 0, 600, 400);
 
-//First Circle
+//Main Face Circle
 theContext.beginPath();
+theContext.fillStyle = '#ffff02'
 theContext.arc(300, 200, 100, 0, Math.PI * 2);
-theContext.moveTo(300, 200);
-theContext.arc(200, 200, 100, 0, Math.PI * 2);
-theContext.stroke();
+theContext.fill() //هذا يقوم بقفل الرسمة تبعتي لهان
+
+//Main Circle
+theContext.beginPath();
+theContext.fillStyle = '#fff'
+theContext.arc(300, 200, 80, 0, Math.PI);
+theContext.fill() //theContext.stroke() //هذا يقوم بقفل الرسمة تبعتي لهان
+
+//Left Eye
+theContext.beginPath();
+theContext.fillStyle = '#000'
+theContext.arc(260, 160, 15, 0, Math.PI * 2);
+theContext.fill()
+
+
+//Right Eye
+theContext.beginPath();
+theContext.fillStyle = '#000'
+theContext.arc(340, 160, 15, 0, Math.PI * 2);
+theContext.fill()
+
+
+//Drow
+// theContext.stroke();
